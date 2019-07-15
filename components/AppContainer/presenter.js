788f2c1788800879269608a2f9dc92506f61a3c9
br j <honuj9983@gmail.com>
2019-07-15 13:35:58 +0900

    Change Styling Head

--- components/AppContainer/presenter.js
+++ components/AppContainer/presenter.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-  Dimensions,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity
-} from "react-native";
+import { Dimensions, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 // import LoggedOutNavigation from "../../navigation/LoggedOutNavigation";
 import ActionNavigation from "../../navigation/ActionNavigation";
 import WebScreen from "../../screens/WebScreen";
@@ -26,7 +20,12 @@ class AppContainer extends Component {
         <StatusBar barStyle={"light-content"} hidden={false} />
         <View style={styles.head}>
           <View style={styles.navi}>
-            <Text style={styles.title} />
+            <Ionicons
+              style={styles.dummy}
+              name="md-bicycle"
+              size={32}
+              color="#193AB7"
+            />
             <Text style={styles.title}>Where is Bike?</Text>
             <WebScreen style={styles.official} />
           </View>
@@ -59,17 +58,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width
   },
-  title: {
+  dummy: {
     flex: 1,
-    paddingTop: 10,
-    paddingBottom: 10,
-    textAlign: "center",
-    alignContent: "center",
+    textAlign: "right"
+  },
+  title: {
+    flex: 2,
+    // paddingTop: 20,
+    // paddingBottom: 20,
+    // textAlign: "center",
+    // alignContent: "center",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    fontSize: 24,
+    fontWeight: "600"
   },
   official: {
     flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
     justifyContent: "center"
   },
   navi: {

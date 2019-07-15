a97872da98ed217745391f1c550fb37a9279ae0b
br j <honuj9983@gmail.com>
2019-07-15 13:34:37 +0900

    Styling Tab Navigation

--- navigation/ActionNavigation.js
+++ navigation/ActionNavigation.js
@@ -1,12 +1,53 @@
+import React from "react";
 import { createAppContainer, createBottomTabNavigator } from "react-navigation";
 import FavoriteScreen from "../screens/FavoriteScreen";
 import TimerScreen from "../screens/TimerScreen";
 import MapScreen from "../screens/MapScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const ActionTabNavigation = createBottomTabNavigator({
-  Favorite: FavoriteScreen,
-  Alram: TimerScreen,
-  Map: MapScreen
+  Favorite: {
+    screen: FavoriteScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => {
+        return (
+          <Ionicons
+            name={focused ? "ios-star" : "ios-star-outline"}
+            size={30}
+            color={focused ? "black" : "gray"}
+          />
+        );
+      }
+    }
+  },
+  Timer: {
+    screen: TimerScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => {
+        return (
+          <Ionicons
+            name={"ios-timer"}
+            size={30}
+            color={focused ? "black" : "gray"}
+          />
+        );
+      }
+    }
+  },
+  Map: {
+    screen: MapScreen,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => {
+        return (
+          <Ionicons
+            name={focused ? "ios-map" : "ios-map"}
+            size={30}
+            color={focused ? "black" : "gray"}
+          />
+        );
+      }
+    }
+  }
 });
 
 export default createAppContainer(ActionTabNavigation);
